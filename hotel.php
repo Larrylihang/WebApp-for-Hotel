b1a9ee481f3abb3143dcb479ad4744b69db27f97
Larry Lee <45256933+Larrylihang@users.noreply.github.com>
2019-04-08 23:10:43 +0800

    Add files via upload
    Here are basic codes for the framework of webapp

---
+++ hotel.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+    <html>
+        <head>
+        <meta charset='utf-8'>
+            <title>Prince Hotel</title>
+            <link rel="stylesheet" type="text/css" href="AI.css"/>  
+        </head>
+        <body>
+            <div id="upper">
+                <ul class='list'>
+                    <li><a href="#">Help</a></li>
+                    <li><a href="#">Login/Join in</a></li>
+                    <li><a href="#">Reserve</a></li>
+                </ul>
+                </div>
+            <div id='bgk'>
+                <h2 class='heading'><b>Prince AI's Luxury Hotel</b>
+                </h2>
+            </div>
+<div id="navigation">
+            <ul class="menu">
+                <li><a href="#">Overview</a></li>
+                <li><a href="#">Rooms</a></li>
+                <li><a href="#">Offers</a></li>
+                <li><a href="#">Meetings</a></li>
+                <li><a href="#">Employees</a></li>
+            </ul>
+            
+</div>
+        
+        
+        </body> 
+
+    
+
+
+    
+    </html>
