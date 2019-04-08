300570f1d3d22c84ede48c43e5ba5f0b712a75ef
Gang944 <45256846+Gang944@users.noreply.github.com>
2019-04-08 23:15:12 +0800

    The interface of the offerings

---
+++ Offerings.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+<meta charset="UTF-8">
+
+<html>
+    <head>
+     <title>Offerings</title>
+     <link rel="stylesheet" href="Offerings.css">
+    </head>
+ <header algin="center"><h1>Offerings</h1></header>
+ <body>   
+    <br>
+    <br>
+    <br>
+     <hr/>
+  <h3 align="center">Special offers and discounts</h3>  
+    <br>
+<ul>
+  <li>
+      <div class="city">
+   <img src="1.jpg" width="30%" height="600">
+   <h2><p>1</p></h2>
+    <p>About 1</p></div>
+          </li>
+  <li>
+    <div class="city">
+   <img src="1.jpg" width="30%" height="600">
+   <h2><p>2</p></h2>
+   <p>About 2</p>
+  </div>
+          </li>
+  <li>
+  <div class="city">
+   <img src="1.jpg" width="30%" height="600">
+   <h2><p>3</p></h2>
+   <p>About 3</p>
+  </div>
+          </li>
+</ul>
+
+ </body>
+
+
+
+</html>
