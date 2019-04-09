559abdd6a6858de0f9d9ec481d25477753257381
Gang944 <45256846+Gang944@users.noreply.github.com>
2019-04-09 12:49:50 +0800

    Update Offerings.php

--- Offerings.php
+++ Offerings.php
@@ -6,38 +6,42 @@
      <title>Offerings</title>
      <link rel="stylesheet" href="Offerings.css">
     </head>
- <header algin="center"><h1>Offerings</h1></header>
+    <header algin="center"><h1><p>Offerings</p></h1></header>
  <body>   
     <br>
     <br>
     <br>
      <hr/>
-  <h3 align="center">Special offers and discounts</h3>  
-    <br>
+  <h3 align="center"><p>Special offers and discounts</p></h3>  
 <ul>
   <li>
       <div class="city">
-   <img src="1.jpg" width="30%" height="600">
-   <h2><p>1</p></h2>
-    <p>About 1</p></div>
+          <img src="1.jpg" width="100%" height="150">
+          <h4 class="text_line"><p>1</p></h4>
+          <h5 class="text_line">About 1</h5>
+      </div>
           </li>
   <li>
-    <div class="city">
-   <img src="1.jpg" width="30%" height="600">
-   <h2><p>2</p></h2>
-   <p>About 2</p>
-  </div>
+      <div class="city">
+          <img src="1.jpg" width="100%" height="150">
+          <h4 class="text_line"><p>1</p></h4>
+          <h5 class="text_line">About 1</h5>
+      </div>
           </li>
   <li>
-  <div class="city">
-   <img src="1.jpg" width="30%" height="600">
-   <h2><p>3</p></h2>
-   <p>About 3</p>
-  </div>
+      <div class="city">
+          <img src="1.jpg" width="100%" height="150">
+          <h4 class="text_line"><p>1</p></h4>
+          <h5 class="text_line">About 1</h5>
+      </div>
           </li>
 </ul>
 
  </body>
+
+
+
+</html>
 
 
 
